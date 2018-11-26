8ea230b207860a8dbbced0e811cc359931bf2cf2
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-11-25 21:30:09 -0800

    first commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
-
-import Dashboard from './components/Dashboard'
-import Edit from './components/Edit'
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    let routes = (
-      <Switch>
-        <Route path='/edit' component={Edit} /> 
-        <Route path='/' exact component={Dashboard} />
-      </Switch>
-    )
     return (
       <div className="App">
-          {routes}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
--- blog/src/index.js
+++ blog/src/index.js
@@ -1,33 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-
-import postsReducer from './store/reducers/postsReducer';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const composeEnhancer =
-  process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
-
-const rootReducer = combineReducers({ postsReducer });
-
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
-
-const app = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
-
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
