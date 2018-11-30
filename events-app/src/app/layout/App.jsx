a8e00316eec9f425b6a95b2ffa93a64a7ae6994b
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-11-30 12:55:46 -0800

    events list

--- events-app/src/app/layout/App.jsx
+++ events-app/src/app/layout/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Event</h1>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
