43840e268a1548f01fdc0fbda1dd598b4be1f020
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-12-01 19:11:45 -0800

    set up redux

---
+++ events-app/src/app/store/configureStore.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../reducers/rootReducer';
+
+export const configureStore = preloadedState => {
+  const middlewares = [];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+  const storeEnhancer = [middlewareEnhancer];
+  const composedEnhancer = composeWithDevTools(...storeEnhancer);
+  const store = createStore(rootReducer, preloadedState, composedEnhancer);
+
+  // Makes changes without refreshing page
+  if (process.env.NODE_ENV !== 'production') {
+    if (module.hot) {
+      module.hot.accept('../reducers/rootReducer', () => {
+        const newRootReducer = require('../reducers/rootReducer').default;
+        store.replaceReducer(newRootReducer)
+      })
+  }
+}
+
+  return store;
+};
