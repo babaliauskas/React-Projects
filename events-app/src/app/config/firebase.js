9708277dfc7fd8bc4702b8c6d74c17c2b17e2a89
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-12-06 20:51:39 -0800

    upload/delete photo

--- events-app/src/app/config/firebase.js
+++ events-app/src/app/config/firebase.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC36VwjVuCaZzWlm5hIbEkcvcALT_njrCM",
-  authDomain: "react-events-fb598.firebaseapp.com",
-  databaseURL: "https://react-events-fb598.firebaseio.com",
-  projectId: "react-events-fb598",
-  storageBucket: "",
-  messagingSenderId: "992091956054"
-}
+  apiKey: 'AIzaSyC36VwjVuCaZzWlm5hIbEkcvcALT_njrCM',
+  authDomain: 'react-events-fb598.firebaseapp.com',
+  databaseURL: 'https://react-events-fb598.firebaseio.com',
+  projectId: 'react-events-fb598',
+  storageBucket: 'react-events-fb598.appspot.com',
+  messagingSenderId: '992091956054'
+};
 
-firebase.initializeApp(firebaseConfig)
-const firestore = firebase.firestore()
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
 const settings = {
   timestampsInSnapshots: true
-}
-firestore.settings(settings)
+};
+firestore.settings(settings);
 
-export default firebase
+export default firebase;
--- events-app/src/features/user/userActions.jsx
+++ events-app/src/features/user/userActions.jsx
@@ -1,5 +1,11 @@
 import moment from 'moment';
 import { toastr } from 'react-redux-toastr';
+import cuid from 'cuid';
+import {
+  asyncActionsStart,
+  asyncActionsFinish,
+  asyncActionsError
+} from '../async/asyncActions';
 
 export const updateProfile = user => {
   return async (dispatch, getState, { getFirebase }) => {
@@ -13,6 +19,83 @@ export const updateProfile = user => {
       toastr.success('Success', 'Profile updated');
     } catch (error) {
       console.log(error);
+    }
+  };
+};
+
+export const uploadProfileImage = (file, fileName) => {
+  return async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const imageName = cuid();
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+    const path = `${user.uid}/user_images`;
+    const options = {
+      name: imageName
+    };
+
+    try {
+      dispatch(asyncActionsStart());
+      let uploadedFile = await firebase.uploadFile(path, file, null, options);
+      let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+      let userDoc = await firestore.get(`users/${user.uid}`);
+      if (!userDoc.data().photoURL) {
+        await firebase.updateProfile({
+          photoURL: downloadURL
+        });
+        await user.updateProfile({
+          photoURL: downloadURL
+        });
+      }
+      await firestore.add(
+        {
+          collection: 'users',
+          doc: user.uid,
+          subcollections: [{ collection: 'photos' }]
+        },
+        {
+          name: imageName,
+          url: downloadURL
+        }
+      );
+      dispatch(asyncActionsFinish());
+    } catch (error) {
+      console.log(error);
+      throw new Error('Problem uploading photo');
+    }
+  };
+};
+
+export const deletePhoto = photo => {
+  return async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+    try {
+      await firebase.deleteFile(`${user.uid}/user_images/${photo.name}`);
+      await firestore.delete({
+        collection: 'users',
+        doc: user.uid,
+        subcollections: [{ collection: 'photos', doc: photo.id }]
+      });
+    } catch (error) {
+      console.log(error);
+      dispatch(asyncActionsError());
+      throw new Error('Problem deleting the photo');
+    }
+  };
+};
+
+export const setMainPhoto = photo => {
+  return async (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    try {
+      return await firebase.updateProfile({
+        photoURL: photo.url
+      });
+    } catch (error) {
+      console.log(error);
+      throw new Error('Problem setting main photo');
     }
   };
 };
